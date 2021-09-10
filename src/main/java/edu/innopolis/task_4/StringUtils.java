32de9a081c1635f9e5d760774eaf9ff4b9a353ed
Ivan-Salnikov <fin.vah@gmail.com>
2021-09-10 06:28:22 +0300

    Done homework 04.

--- src/main/java/edu/innopolis/task_4/StringUtils.java
+++ src/main/java/edu/innopolis/task_4/StringUtils.java
@@ -1,5 +1,40 @@
 package edu.innopolis.task_4;
 
-public interface StringUtils {
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringUtils {
+    private String inputString;
+    private Map<Character, Integer> stringSequencesMap;
+
+    public StringUtils(String inputString, Map<Character, Integer> stringSequencesMap) {
+        this.inputString = inputString;
+        this.stringSequencesMap = stringSequencesMap;
+        fillStringSequencesMap ();
+    }
+
+    public StringUtils(String inputString) {
+        this(inputString, new HashMap<>());
+    }
+
+    public Map<Character, Integer> getStringSequencesMap () {
+        return stringSequencesMap;
+    }
+
+    public void fillStringSequencesMap () {
+        char[] strToArray = inputString.toCharArray();
+        for (int i = 0; i < strToArray.length; i++) {
+            if(stringSequencesMap.containsKey(strToArray[i])) {
+                stringSequencesMap.put(strToArray[i], stringSequencesMap.get(strToArray[i])+1);
+            } else {
+                stringSequencesMap.put(strToArray[i], 1);
+            }
+        }
+    }
+
+    public String getMapToString () {
+        return stringSequencesMap.toString();
+    }
+
 
 }
--- src/main/java/edu/innopolis/task_4/StringUtilsImpl.java
+++
@@ -1,9 +0,0 @@
-package edu.innopolis.task_4;
-
-import java.util.Map;
-
-public class StringUtilsImpl implements StringUtils {
-    private String inputString;
-    private Map<Character, Integer> stringSequencesMap;
-
-}
